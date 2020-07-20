44508da2f5f30901734d78e54f4b57bc5c4e2a22
Gise Ares <gise.ares@gmail.com>
2020-07-20 14:42:08 -0300

    respuesta en answer

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,18 +1,15 @@
 class QuestionsController < ApplicationController
   def ask
-    #if params[:question] != ""
-      # habria que redireccionar
-     # <%= link_to "envio a pagina de respuesta", http://localhost:3000/answer %>
   end
 
   def answer
+    @question = params[:pregunta]
+    if params[:pregunta] == "I am going to work"
+      @answer =  "Great!"
+    elsif params[:pregunta].include?("?")
+      @answer ="Silly question, get dressed and go to work!."
+    else
+      @answer ="I don't care, get dressed and go to work!"
+    end 
   end
-
 end
-
-
-# 1. Un lugar donde un usuario una pregunta 
-# 2. Tomar la pregunta (params) 
-# 3. Redireccionar a una nueva ruta
-# 4. Printear la pregunta
-# 5. Printear la respuesta del coach
