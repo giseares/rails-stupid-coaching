96ac169adad94f89b4ccf3bf0a7b460db341e0fc
Gise Ares <gise.ares@gmail.com>
2020-07-20 13:16:29 -0300

    redireccionar la pregunta a answer

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,13 +1,17 @@
 class QuestionsController < ApplicationController
   def ask
+    #if params[:question] != ""
+      # habria que redireccionar
+     # <%= link_to "envio a pagina de respuesta", http://localhost:3000/answer %>
   end
 
   def answer
   end
+
 end
 
 
-# 1. Un lugar donde un usuario una pregunta
+# 1. Un lugar donde un usuario una pregunta 
 # 2. Tomar la pregunta (params) 
 # 3. Redireccionar a una nueva ruta
 # 4. Printear la pregunta
